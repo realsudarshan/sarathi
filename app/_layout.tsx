a9ee3ba94d30556693f84d862fb82e7f67bfddad
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-11-19 10:32:50 +0545

    ready for eas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,23 @@ import { Stack } from "expo-router";
 import "../global.css";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  useEffect(() => {
+    // Hide the splash screen once the app is ready
+    const hideSplash = async () => {
+      await SplashScreen.hideAsync();
+    };
+    
+    // Delay to show splash screen for 3 seconds
+    const timer = setTimeout(hideSplash, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
   <ClerkProvider tokenCache={tokenCache}>
