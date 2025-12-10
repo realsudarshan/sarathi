43f75193eaac4c3f69d63235fcc8c24f3b12ebd6
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2025-12-10 21:21:36 +0545

    fix(auth): publishable key of clerk added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,9 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from "expo-router";
-import "../global.css";
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import { useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import "../global.css";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
@@ -14,20 +14,28 @@ export default function RootLayout() {
     const hideSplash = async () => {
       await SplashScreen.hideAsync();
     };
-    
+
     // Delay to show splash screen for 3 seconds
     const timer = setTimeout(hideSplash, 1000);
     return () => clearTimeout(timer);
   }, []);
 
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+  if (!publishableKey) {
+    throw new Error(
+      'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
+    )
+  }
+
   return (
-  <ClerkProvider tokenCache={tokenCache}>
-    <Stack>
-    <Stack.Screen name="index" ></Stack.Screen>
-    <Stack.Screen name="(auth)" options={{headerShown:false}}></Stack.Screen>
-    <Stack.Screen name="(root)" options={{headerShown:false}}></Stack.Screen>
-    <Stack.Screen name="+not-found"></Stack.Screen>
-   
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <Stack>
+        <Stack.Screen name="index" ></Stack.Screen>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name="(root)" options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name="+not-found"></Stack.Screen>
+
+      </Stack>
     </ClerkProvider>) //take whatever .tsx files are inside the app/ folder and make them screens
 }
